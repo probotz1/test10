3281bd8a9fe2981ad1b5f539522bb8ba60290f73
ashwinrin77 <161035206+ashwinrin77@users.noreply.github.com>
2024-07-04 17:46:10 -1200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
-
+api_id = '28015531'  # replace with your api_id
+api_hash = '2ab4ba37fd5d9ebf1353328fc915ad28'  # replace with your api_hash
+bot_token = '7218885939:AAE-IXkH5XG_pF5GNV41MvFRqW3LWwqeaOw'  # replace with your bot token
